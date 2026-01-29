9e4df52fc9bbb97b56b9946f7560f8fe3fae20d4
JDBLE <jarrod.dumble@gmail.com>
2026-01-30 06:59:47 +1100

    Added basic inference

--- src/lib/open-router.ts
+++ src/lib/open-router.ts
@@ -6,12 +6,23 @@ const openai = new OpenAI({
 })
 
 export async function openRounterInference() {
+
+
   const completion = await openai.chat.completions.create({
     model: "arcee-ai/trinity-large-preview:free",
     messages: [
       { role: "user", content: "Say this is a test, in Italian" }
     ],
   })
+
+  const response = await openai.responses.create({
+    model: "arcee-ai/trinity-large-preview:free",
+    reasoning: { effort: "low" },
+    instructions: "Talk like a pirate.",
+    input: "Are semicolons optional in JavaScript?",
+  });
+
+  console.log(response.output_text);
 
   return completion.choices[0].message.content
 }
